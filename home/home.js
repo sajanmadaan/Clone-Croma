f23976ed91ae5d550985217174b923d5ed00f1fd
anshul160196 <93374095+anshul160196@users.noreply.github.com>
2021-11-03 02:14:50 +0530

    slide indicator

--- home/home.js
+++ home/home.js
@@ -4,25 +4,28 @@ showSlides(slideIndex);
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  console.log(dots.length)
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every seconds
+  dots[slideIndex-1].className += " active";
+  setInterval(showSlides, 6000); // Change image every seconds
 }
 
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
-    showSlides(slideIndex);
+    showSlides(n);
 }
 
-function currentSlides(n) {
-    console.log(n)
-    showSlides(slideIndex = n)
+function currentSlide(n) {
+    showSlides(n)
 }
