5bf3554a6b5cd132620bd490b780222d9d44fba5
KundanPrasad11 <prasadkundan2019@gmail.com>
2021-11-11 00:38:57 +0530

    Update slider.js

--- mobile_products/slider.js
+++ mobile_products/slider.js
@@ -23,4 +23,6 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+    slideIndex++;
+    setTimeout(showSlides, 6000);
 }
