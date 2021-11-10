1ea4a87fe58c8794185f4befaf7cccdca2d010e6
anshul160196 <93374095+anshul160196@users.noreply.github.com>
2021-11-11 01:17:19 +0530

    making of popular items page

---
+++ popularItems/popularItems.js
@@ -0,0 +1,169 @@
+popularItems = [
+  {
+    name: "Xiaomi Redmi Note 10 Pro (128GB ROM, 8GB RAM, MZB08WUIN, Glacial Blue)",
+    id: 233743,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1616503004/Croma%20Assets/Communication/Mobiles/Images/233743_iportt.png/mxw_2048,f_auto",
+    price: "₹19,499.00",
+    realPrice: "₹21,999.00",
+    discount: "11% Off",
+    rating: "4.5",
+  },
+  {
+    name: "Xiaomi Redmi Note 10 Pro (128GB ROM, 8GB RAM, MZB08WUIN, Vintage Bronze)",
+    id: 233742,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1616503004/Croma%20Assets/Communication/Mobiles/Images/233743_iportt.png/mxw_2048,f_auto",
+    price: "₹19,499.00",
+    realPrice: "₹21,999.00",
+    discount: "11% Off",
+    rating: "4.5",
+  },
+  {
+    name: "Xiaomi Redmi Note 10 Pro (128GB ROM, 6GB RAM, MZB08WUIN, Glacial Blue)",
+    id: 233739,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1616503004/Croma%20Assets/Communication/Mobiles/Images/233743_iportt.png/mxw_2048,f_auto",
+    price: "₹19,499.00",
+    realPrice: "₹21,999.00",
+    discount: "11% Off",
+    rating: "4.5",
+  },
+  {
+    name: "OnePlus Nord CE 5G (256GB ROM, 12GB RAM, Charcoal Ink)",
+    id: 235152,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1623389445/Croma%20Assets/Communication/Mobiles/Images/235152_aush7p.png/mxw_2048,f_auto",
+    price: "₹25,199.00",
+    realPrice: "₹27,999.00",
+    discount: "10% Off",
+    rating: "4",
+  },
+  {
+    name: "Xiaomi Redmi Note 10 Pro Max (128GB ROM, 6GB RAM, MZB08LUIN, Dark Night)",
+    id: 233747,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1616517842/Croma%20Assets/Communication/Mobiles/Images/233747_wsju8u.png/mxw_2048,f_auto",
+    price: "₹20,499.00",
+    realPrice: "₹22,999.00",
+    discount: "11% Off",
+    rating: "3.5",
+  },
+  {
+    name: "Vivo Y73 (128GB ROM, 8GB RAM, PD2078F_IN, Diamond Flare)",
+    id: 234749,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1623164395/Croma%20Assets/Communication/Mobiles/Images/234749_nbsxxn.png/mxw_2048,f_auto",
+    price: "₹20,990.00",
+    realPrice: "₹24,990.00",
+    discount: "16% Off",
+    rating: "4.5",
+  },
+  {
+    name: "Xiaomi Mi 11 Lite (128GB ROM, 6GB RAM, MZB08IOIN, Tuscany Coral)",
+    id: 237421,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1624887481/Croma%20Assets/Communication/Mobiles/Images/237421_m6dsju.png/mxw_2048,f_auto",
+    price: "₹21,999.00",
+    realPrice: "₹24,999.00",
+    discount: "12% Off",
+    rating: "4.3",
+  },
+  {
+    name: "Oppo A53s (128GB ROM, 8GB RAM, CPH2321, Crystal Blue)",
+    id: 234468,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1622813035/Croma%20Assets/Communication/Mobiles/Images/234468_ntlstl.png/mxw_2048,f_auto",
+    price: "₹16,990.00",
+    orginal_price: "₹18,990.00",
+    discount: "11% Off",
+    rating: "4.5",
+  },
+  {
+    name: "Vivo V21e (128GB ROM, 8GB RAM, PD2102BF_IN, Sunset Jazz)",
+    id: 235454,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1624523026/Croma%20Assets/Communication/Mobiles/Images/235454_idgeca.png/mxw_2048,f_auto",
+    price: "₹24,990.00",
+    realPrice: "₹27,990.00",
+    discount: "11% Off",
+    rating: "4",
+  },
+  {
+    name: "Realme 8 5G (128GB ROM, 8GB RAM, RMX3241, Supersonic Blue)",
+    id: 234442,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1620113698/Croma%20Assets/Communication/Mobiles/Images/234442_aylud4.png/mxw_2048,f_auto",
+    price: "₹18,499.00",
+    realPrice: "₹18,999.00",
+    discount: "3% Off",
+    rating: "4.5",
+  },
+  {
+    name: "Oppo A74 (128GB ROM, 6GB RAM, CPH2263, Fluid Black)",
+    id: 234265,
+    image_url:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1618983581/Croma%20Assets/Communication/Mobiles/Images/234265_qtch9x.png/mxw_2048,f_auto",
+    price: "₹17,990.00",
+    orginal_price: "₹20,990.00",
+    discount: "14% Off",
+    rating: "4.5",
+  },
+];
+
+var body = document.querySelector("body");
+var popularMainContainer = document.getElementById("popularMainContainer");
+var productContainer = document.getElementById("productContainer");
+
+popularProd(popularItems);
+function popularProd(data) {
+  data.map(function (item) {
+    var prodBox = document.getElementById("productBox");
+    // box for item
+    var box = document.createElement("div");
+    box.setAttribute("id", "box");
+    prodBox.append(box);
+    var img = document.createElement("img");
+    img.setAttribute("id", "productImage");
+    img.src = item.image_url;
+    // box for product description
+    var descriptionBox = document.createElement("div");
+    descriptionBox.setAttribute("id", "desBox");
+    var prodDes = document.createElement("h2");
+    prodDes.setAttribute("id", "prodDes");
+    prodDes.textContent = item.name;
+    var id = document.createElement("p");
+    id.setAttribute("id", "prodId");
+    id.textContent = "Product-Id: " + item.id;
+    //  price line
+    var prodPrice = document.createElement("div");
+    prodPrice.setAttribute("id", "prodPrice");
+
+    var discPrice = document.createElement("span");
+    discPrice.setAttribute("id", "discPrice");
+    discPrice.textContent = item.price;
+    var realPrice = document.createElement("span");
+    realPrice.setAttribute("id", "realPrice");
+    realPrice.textContent = item.realPrice;
+    var discount = document.createElement("span");
+    discount.setAttribute("id", "discount");
+    discount.textContent = item.discount;
+    var star = document.createElement("span");
+    star.setAttribute("id", "star");
+    star.textContent = item.rating;
+
+    var compareBox = document.createElement("div");
+    compareBox.setAttribute("id","compareBox");
+    var checkBox = document.createElement("input");
+    checkBox.type = "checkbox"
+
+    // appending data in body
+    compareBox.append(checkBox);
+    prodPrice.append(discPrice, realPrice, discount, star);
+    descriptionBox.append(prodDes, id, prodPrice,compareBox);
+    box.append(img, descriptionBox);
+    productContainer.append(prodBox);
+    popularMainContainer.append(productContainer);
+    body.append(popularMainContainer);
+  });
+}
