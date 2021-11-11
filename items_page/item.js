4ee571a357618971446b560df314f32c48991d42
KundanPrasad11 <prasadkundan2019@gmail.com>
2021-11-12 02:14:46 +0530

    created the right side of the items page
    left side will be updated in the morning.
    addtocart and buy now functionalities are left .

---
+++ items_page/item.js
@@ -0,0 +1,441 @@
+ var data = [
+    
+    {
+      name: "Vivo X70 Pro+ 5G (256GB ROM, 12GB RAM, V2114, Enigma Black)",
+      id: 244201,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632899386/Croma%20Assets/Communication/Mobiles/Images/244201_bjxdfq.png/mxw_1440,f_auto",
+      price: 79990.00 ,
+      realPrice: 84990 ,
+      discount: "6% Off",
+      rating: 5.0,
+    },
+    
+    {
+      name: "Vivo X70 Pro 5G (128GB ROM, 8GB RAM, V2105, Aurora Dawn)",
+      id: 244200,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632896987/Croma%20Assets/Communication/Mobiles/Images/244200_zfdd21.png/mxw_1440,f_auto",
+      price: 46990.00,
+      realPrice: 51990.00 ,
+      discount: "10% Off",
+      rating: 5.0,
+    },
+    
+    {
+      name: "Vivo X70 Pro 5G (128GB ROM, 8GB RAM, V2105, Cosmic Black)",
+      id: 244199,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632897825/Croma%20Assets/Communication/Mobiles/Images/244199_kfo03v.png/mxw_1440,f_auto",
+      price: 46990.00,
+      realPrice: 51990.00 ,
+      discount: "10% Off",
+      rating: 4.5,
+    },
+    
+    {
+      name: "Vivo X70 Pro 5G (256GB ROM, 8GB RAM, V2105, Aurora Dawn)",
+      id: 244198,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632897825/Croma%20Assets/Communication/Mobiles/Images/244199_kfo03v.png/mxw_1440,f_auto",
+      price: 46990.00,
+      realPrice: 54990.00 ,
+      discount: "9% Off",
+      rating: 5.0,
+    },
+    
+    {
+      name: "Vivo X70 Pro 5G (256GB ROM, 12GB RAM, V2105, Aurora Dawn)",
+      id: 244196,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632897825/Croma%20Assets/Communication/Mobiles/Images/244199_kfo03v.png/mxw_1440,f_auto",
+      price: 52990.00,
+      realPrice: 57990.00 ,
+      discount: "9% Off",
+      rating: 4.0,
+    },
+    
+    {
+      name: "Vivo X70 Pro 5G (256GB ROM, 8GB RAM, V2105, Cosmic Black)",
+      id: 244197,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632897765/Croma%20Assets/Communication/Mobiles/Images/244197_bebtmy.png/mxw_1440,f_auto",
+      price: 49990.00,
+      realPrice: 54990.00 ,
+      discount: "9% Off",
+      rating: 5.0,
+    },
+    
+    {
+      name: "Vivo X70 Pro 5G (256GB ROM, 12GB RAM, V2105, Cosmic Black)",
+      id: 244195,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632897713/Croma%20Assets/Communication/Mobiles/Images/244195_hz95yq.png/mxw_1440,f_auto",
+      price: 52990.00,
+      realPrice: 57990.00 ,
+      discount: "9% Off",
+      rating: 4.5,
+    },
+    
+    {
+      name: "Oppo F19s (128GB ROM, 6GB RAM, CPH2223, Glowing Gold)",
+      id: 243544,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632297167/Croma%20Assets/Communication/Mobiles/Images/243544_mew1yg.png/mxw_1440,f_auto",
+      price: 19990.00,
+      realPrice: 22999.00 ,
+      discount: "13% Off",
+      rating: 5.0,
+    },
+    
+    {
+      name: "Oppo F19s (128GB ROM, 6GB RAM, CPH2223, Glowing Black)",
+      id: 243543,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632297167/Croma%20Assets/Communication/Mobiles/Images/243544_mew1yg.png/mxw_1440,f_auto",
+      price: 19990.00,
+      realPrice: 22999.00 ,
+      discount: "13% Off",
+      rating: 4.8,
+    },
+    
+    {
+      name: "Apple iPhone 13 (512GB ROM, MLQG3HN/A, Blue)",
+      id: 243476,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631798146/Croma%20Assets/Communication/Mobiles/Images/243476_yfkl9n.png/mxw_1440,f_auto",
+      price: 109990.00,
+      rating: 4.0,
+    },
+    
+    {
+      name: "Apple iPhone 13 (256GB ROM, MLQA3HN/A, Blue)",
+      id: 243468,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631798146/Croma%20Assets/Communication/Mobiles/Images/243476_yfkl9n.png/mxw_1440,f_auto",
+      price: 89900.00,
+      rating: 4.0,
+    },
+    
+    {
+      name: "Apple iPhone 13 (256GB ROM, MLQ93HN/A, (Product)Red)",
+      id: 243467,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631776822/Croma%20Assets/Communication/Mobiles/Images/243467_r6n0yh.png/mxw_1440,f_auto",
+      price: 89900.00,
+      rating: 4.0,
+    },
+    
+    {
+      name: "Apple iPhone 13 (512GB ROM, MLQF3HN/A, (Product)Red)",
+      id: 243472,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631776822/Croma%20Assets/Communication/Mobiles/Images/243467_r6n0yh.png/mxw_1440,f_auto",
+      price: 109900.00,
+      rating: 4.0,
+    },
+    
+    {
+      name: "Apple iPhone 13 (512GB ROM, MLQE3HN/A, Pink)",
+      id: 243471,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631814830/Croma%20Assets/Communication/Mobiles/Images/243471_rskpqq.png/mxw_1440,f_auto",
+      price: 109900.00,
+      rating: 4.0,
+    },
+    
+    {
+      name: "Apple iPhone 13 (512GB ROM, MLQC3HN/A, Midnight Black)",
+      id: 243469,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631799707/Croma%20Assets/Communication/Mobiles/Images/243469_lfvpcu.png/mxw_1440,f_auto",
+      price: 109900.00,
+      rating: 4.0,
+    },
+    
+    {
+      name: "Apple iPhone 13 (512GB ROM, MLQD3HN/A, Starlight White)",
+      id: 243470,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631799865/Croma%20Assets/Communication/Mobiles/Images/243470_zpaswv.png/mxw_1440,f_auto",
+      price: 109900.00,
+      rating: 4.0,
+    },
+    
+    {
+      name: "Apple iPhone 13 (256GB ROM, MLQ83HN/A, Pink)",
+      id: 243466,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631797995/Croma%20Assets/Communication/Mobiles/Images/243466_jrcp08.png/mxw_1440,f_auto",
+      price: 89900.00,
+      rating: 4.0,
+    },
+    
+    {
+      name: "Apple iPhone 13 (256GB ROM, MLQ73HN/A, Starlight White)",
+      id: 243465,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631797300/Croma%20Assets/Communication/Mobiles/Images/243465_scozk2.png/mxw_1440,f_auto",
+      price: 89900.00,
+      rating: 4.5,
+    },
+    
+    {
+      name: "Apple iPhone 13 (256GB ROM, MLQ63HN/A, Midnight Black)",
+      id: 243464,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631776732/Croma%20Assets/Communication/Mobiles/Images/243464_in6uf5.png/mxw_1440,f_auto",
+      price: 89900.00,
+      rating: 4.5,
+    },
+    
+    {
+      name: "Apple iPhone 13 (128GB ROM, MLPK3HN/A, Blue)",
+      id: 243463,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631776566/Croma%20Assets/Communication/Mobiles/Images/243463_wwwi8s.png/mxw_1440,f_auto",
+      price: 79900.00,
+      rating: 4.1,
+    },
+    
+    {
+      name: "Apple iPhone 13 (128GB ROM, MLPG3HN/A, Starlight White)",
+      id: 243460,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631773696/Croma%20Assets/Communication/Mobiles/Images/243460_yi4yqb.png/mxw_1440,f_auto",
+      price: 79900.00,
+      rating: 4.5,
+    },
+    
+    {
+      name: "Apple iPhone 13 (128GB ROM, MLPJ3HN/A, (Product)Red)",
+      id: 243462,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631773948/Croma%20Assets/Communication/Mobiles/Images/243462_x2j1be.png/mxw_1440,f_auto",
+      price: 79900.00,
+      rating: 4.5,
+    },
+    
+    {
+      name: "Apple iPhone 13 (128GB ROM, MLPH3HN/A, Pink)",
+      id: 243461,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631773796/Croma%20Assets/Communication/Mobiles/Images/243461_tgipil.png/mxw_1440,f_auto",
+      price: 79900.00,
+      rating: 4.1,
+    },
+    
+    {
+      name: "Apple iPhone 13 (128GB ROM, MLPF3HN/A, Midnight Black)",
+      id: 243459,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631773619/Croma%20Assets/Communication/Mobiles/Images/243459_yasser.png/mxw_1440,f_auto",
+      price: 79900.00,
+      rating: 4.1,
+    },
+    
+    {
+      name: "OnePlus Nord 2 5G (256GB ROM, 12GB RAM, DN2101, Grey Sierra)",
+      id: 240338,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1627023024/Croma%20Assets/Communication/Mobiles/Images/240338_mtffrr.png/mxw_1440,f_auto",
+      price: 34999.00,
+      rating: 4.2,
+    },
+    
+    {
+      name: "OnePlus Nord 2 5G (256GB ROM, 12GB RAM, DN2101, Blue Haze)",
+      id: 240336,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1627022219/Croma%20Assets/Communication/Mobiles/Images/240336_pcqerd.png/mxw_1440,f_auto",
+      price: 34999.00,
+      rating: 4.5,
+    },
+    
+    {
+      name: "Oppo Reno 6 5G (128GB ROM, 8GB RAM, CPH2251, Aurora)",
+      id: 238811,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1626327626/Croma%20Assets/Communication/Mobiles/Images/238811_c8td57.png/mxw_1440,f_auto",
+      price: 29990.00,
+      realPrice: 35990.00 ,
+      discount: "17% Off",
+      rating: 4.2,
+    },
+    
+    {
+      name: "Oppo Reno 6 Pro 5G (256GB ROM, 12GB RAM, CPH2249, Stellar Black)",
+      id: 238812,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1626327700/Croma%20Assets/Communication/Mobiles/Images/238812_laaysh.png/mxw_1440,f_auto",
+      price: 39990.00,
+      realPrice: 46990.00 ,
+      discount: "15% Off",
+      rating: 4.5,
+    },
+    
+    {
+      name: "Oppo Reno 6 Pro 5G (256GB ROM, 12GB RAM, CPH2251, Aurora)",
+      id: 238810,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1626415015/Croma%20Assets/Communication/Mobiles/Images/238810_ljiygl.png/mxw_1440,f_auto",
+      price: 39990.00,
+      realPrice: 46990.00 ,
+      discount: "15% Off",
+      rating: 4.2,
+    },
+    
+    {
+      name: "Vivo Y72 5G (128GB ROM, 8GB RAM, PD2112F_EX, Prism Magic)",
+      id: 238735,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1626163961/Croma%20Assets/Communication/Mobiles/Images/238735_pnnskt.png/mxw_1440,f_auto",
+      price: 20990.00,
+      realPrice: 24990.00 ,
+      discount: "16% Off",
+      rating: 4.5,
+    },
+    
+    {
+      name: "Vivo Y72 5G (128GB ROM, 8GB RAM, PD2112F_EX, Slate Grey)",
+      id: 238734,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1626163867/Croma%20Assets/Communication/Mobiles/Images/238734_ztg5ma.png/mxw_1440,f_auto",
+      price: 20990.00,
+      realPrice: 24990.00 ,
+      discount: "16% Off",
+      rating: 3.9,
+    },
+    
+    {
+      name: "Vivo Y1s (32GB ROM, 3GB RAM, PD2014CF_EX, Aurora Blue)",
+      id: 237897,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1625830739/Croma%20Assets/Communication/Mobiles/Images/237897_iwgpj9.png/mxw_1440,f_auto",
+      price: 9490.00,
+      realPrice: 11990.00 ,
+      discount: "21% Off",
+      rating: 4.0,
+    },
+    
+    {
+      name: "Vivo Y1s (32GB ROM, 3GB RAM, PD2014CF_EX, Olive Black)",
+      id: 237896,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1625830739/Croma%20Assets/Communication/Mobiles/Images/237897_iwgpj9.png/mxw_1440,f_auto",
+      price: 9490.00,
+      realPrice: 20990.00 ,
+      discount: "19% Off",
+      rating: 4.9,
+    },
+    
+    {
+      name: "Vivo Y51A (128GB ROM, 6GB RAM, PD2050KF_IN, Titanium Sapphire)",
+      id: 235854,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1626176505/Croma%20Assets/Communication/Mobiles/Images/235854_b6wt0h.png/mxw_1440,f_auto",
+      price: 16999.00,
+      realPrice: 20990.00 ,
+      discount: "19% Off",
+      rating: 4.1,
+    },
+    
+    {
+      name: "Vivo Y51A (128GB ROM, 6GB RAM, PD2050KF_IN, Crystal Symphony)",
+      id: 235855,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1626176591/Croma%20Assets/Communication/Mobiles/Images/235855_s30ukw.png/mxw_1440,f_auto",
+      price: 16999.00,
+      realPrice: 20990.00 ,
+      discount: "19% Off",
+      rating: 5.0,
+    },
+    
+    {
+      name: "Xiaomi Mi 11 Lite (128GB ROM, 8GB RAM, MZB08IPIN, Tuscany Coral)",
+      id: 237424,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1624887797/Croma%20Assets/Communication/Mobiles/Images/237424_n1rw9r.png/mxw_1440,f_auto",
+      price: 23999.00,
+      realPrice: 25999.00 ,
+      discount: "8% Off",
+      rating: 4.0,
+    },
+    
+    {
+      name: "Xiaomi Mi 11 Lite (128GB ROM, 8GB RAM, MZB08IRIN, Jazz Blue)",
+      id: 237423,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1624887719/Croma%20Assets/Communication/Mobiles/Images/237423_y7lpv2.png/mxw_1440,f_auto",
+      price: 23999.00,
+      realPrice: 25999.00 ,
+      discount: "8% Off",
+      rating: 4.0,
+    },
+    
+    {
+      name: "Xiaomi Mi 11 Lite (128GB ROM, 8GB RAM, MZB08ININ, Vinyl Black)",
+      id: 237422,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1624887719/Croma%20Assets/Communication/Mobiles/Images/237423_y7lpv2.png/mxw_1440,f_auto",
+      price: 23999.00,
+      realPrice: 25999.00 ,
+      discount: "8% Off",
+      rating: 4.0,
+    },
+    
+    {
+      name: "Xiaomi Mi 11 Lite (128GB ROM, 6GB RAM, MZB08IOIN, Tuscany Coral)",
+      id: 237421,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1624887481/Croma%20Assets/Communication/Mobiles/Images/237421_m6dsju.png/mxw_1440,f_auto",
+      price: 21999.00,
+      realPrice: 24999.00 ,
+      discount: "12% Off",
+      rating: 4.9,
+    },
+    
+    {
+      name: "Xiaomi Mi 11 Lite (128GB ROM, 6GB RAM, MZB08IQIN, Jazz Blue)",
+      id: 237420,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1624887395/Croma%20Assets/Communication/Mobiles/Images/237420_e1fkja.png/mxw_1440,f_auto",
+      price: 21999.00,
+      realPrice: 24999.00 ,
+      discount: "12% Off",
+      rating: 4.0,
+    },   
+    
+    {
+      name: "Xiaomi Mi 11 Lite (128GB ROM, 6GB RAM, MZB089LIN, Vinyl Black)",
+      id: 237419,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1624887307/Croma%20Assets/Communication/Mobiles/Images/237419_btpank.png/mxw_1440,f_auto",
+      price: 21999.00,
+      realPrice: 24999.00 ,
+      discount: "12% Off",
+      rating: 4.0,
+    },   
+    
+    {
+      name: "Vivo V21e (128GB ROM, 8GB RAM, PD2102BF_IN, Sunset Jazz)",
+      id: 235454,
+      image_url:
+        "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1624523026/Croma%20Assets/Communication/Mobiles/Images/235454_idgeca.png/mxw_1440,f_auto",
+      price: 24990.00,
+      realPrice: 27990.00 ,
+      discount: "11% Off",
+      rating: 4.1,
+    },   
+];
+var item = data[0];
+console.log(item);
+var nam = document.getElementById("nam");
+nam.textContent = item.name;
+var prod_id = document.getElementById("prod_id");
+prod_id.textContent = `PRODUCT ID : ${item.id}`;
+var price = document.getElementById("payable");
+price.innerHTML = `<span>&#8377;${item.price}<\span>`;
+var realPrice = document.getElementById("mrp");
+realPrice.innerHTML = `<span>${item.realPrice}<\span>`;
+
